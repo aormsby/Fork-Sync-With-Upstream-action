7a73fa3517cd631c0e34db0a4c3cb4426e104bf4
Adam Ormsby <ormsbyadam@gmail.com>
2020-12-22 20:07:27 -0600

    Remove config_git_credentials input (not needed)

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -6,53 +6,48 @@ set -e
 
 # set user credentials in git config
 config_git() {
-    if [ "${INPUT_CONFIG_GIT_CREDENTIALS}" = true ]; then
-        # store original user config for reset later
-        ORIG_USER=$(git config --global --get --default="null" user.name)
-        ORIG_EMAIL=$(git config --global --get --default="null" user.email)
-        ORIG_PULL_CONFIG=$(git config --global --get --default="null" pull.rebase)
+    # store original user config for reset later
+    ORIG_USER=$(git config --global --get --default="null" user.name)
+    ORIG_EMAIL=$(git config --global --get --default="null" user.email)
+    ORIG_PULL_CONFIG=$(git config --global --get --default="null" pull.rebase)
 
-        if [ "${INPUT_GIT_EMAIL}" != "null" ]; then
-            git config --global user.name "${INPUT_GIT_EMAIL}"
-        fi
-
-        if [ "${INPUT_GIT_USER}" != "null" ]; then
-            git config --global user.email "${INPUT_GIT_USER}"
-        fi
-
-        if [ "${INPUT_GIT_PULL_REBASE_CONFIG}" != "null" ]; then
-            git config --global pull.rebase "${INPUT_GIT_PULL_REBASE_CONFIG}"
-        fi
-
-        echo 'Git user and email credentials set for action' 1>&1
+    if [ "${INPUT_GIT_EMAIL}" != "null" ]; then
+        git config --global user.name "${INPUT_GIT_EMAIL}"
     fi
+
+    if [ "${INPUT_GIT_USER}" != "null" ]; then
+        git config --global user.email "${INPUT_GIT_USER}"
+    fi
+
+    if [ "${INPUT_GIT_PULL_REBASE_CONFIG}" != "null" ]; then
+        git config --global pull.rebase "${INPUT_GIT_PULL_REBASE_CONFIG}"
+    fi
+
+    echo 'Git user and email credentials set for action' 1>&1
 }
 
 # reset user credentials to originals
 reset_git() {
-    if [ "${INPUT_CONFIG_GIT_CREDENTIALS}" = true ]; then
-    
-        if [ "${ORIG_USER}" == "null" ]; then
-            git config --global --unset user.name
-        else
-            git config --global user.name "${ORIG_USER}"
-        fi
-
-        if [ "${ORIG_EMAIL}" == "null" ]; then
-            git config --global --unset user.email
-        else
-            git config --global user.email "${ORIG_EMAIL}"
-        fi
-
-        if [ "${ORIG_PULL_CONFIG}" == "null" ]; then
-            git config --global --unset pull.rebase
-        else
-            git config --global pull.rebase "${ORIG_PULL_CONFIG}"
-        fi
-
-        echo 'Git user name and email credentials reset to original state' 1>&1
-        echo 'Git pull config reset to original state' 1>&1
+    if [ "${ORIG_USER}" == "null" ]; then
+        git config --global --unset user.name
+    else
+        git config --global user.name "${ORIG_USER}"
     fi
+
+    if [ "${ORIG_EMAIL}" == "null" ]; then
+        git config --global --unset user.email
+    else
+        git config --global user.email "${ORIG_EMAIL}"
+    fi
+
+    if [ "${ORIG_PULL_CONFIG}" == "null" ]; then
+        git config --global --unset pull.rebase
+    else
+        git config --global pull.rebase "${ORIG_PULL_CONFIG}"
+    fi
+
+    echo 'Git user name and email credentials reset to original state' 1>&1
+    echo 'Git pull config reset to original state' 1>&1
 }
 
 ### functions above ###
