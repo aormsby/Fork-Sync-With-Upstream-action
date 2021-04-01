33d8d54fdde94e5f1566c71cef2fd1b205883c9d
Stewart Peace <stewart@kantan.tech>
2021-04-01 21:14:18 +0100

    .

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -1,92 +1,125 @@
 #!/bin/sh
 
-set -e
-# do not quote GIT_PULL_ARGS or GIT_*_ARGS. As they may contain
-# more than one argument.
-
-# set user credentials in git config
-config_git() {
-    # store original user config for reset later
-    ORIG_USER=$(git config --global --get --default="null" user.name)
-    ORIG_EMAIL=$(git config --global --get --default="null" user.email)
-    ORIG_PULL_CONFIG=$(git config --global --get --default="null" pull.rebase)
-
-    if [ "${INPUT_GIT_USER}" != "null" ]; then
-        git config --global user.name "${INPUT_GIT_USER}"
-    fi
-
-    if [ "${INPUT_GIT_EMAIL}" != "null" ]; then
-        git config --global user.email "${INPUT_GIT_EMAIL}"
-    fi
-
-    if [ "${INPUT_GIT_PULL_REBASE_CONFIG}" != "null" ]; then
-        git config --global pull.rebase "${INPUT_GIT_PULL_REBASE_CONFIG}"
-    fi
-
-    echo 'Git user and email credentials set for action' 1>&1
-}
-
-# reset user credentials to originals
-reset_git() {
-    if [ "${ORIG_USER}" = "null" ]; then
-        git config --global --unset user.name
-    else
-        git config --global user.name "${ORIG_USER}"
-    fi
-
-    if [ "${ORIG_EMAIL}" = "null" ]; then
-        git config --global --unset user.email
-    else
-        git config --global user.email "${ORIG_EMAIL}"
-    fi
-
-    if [ "${ORIG_PULL_CONFIG}" = "null" ]; then
-        git config --global --unset pull.rebase
-    else
-        git config --global pull.rebase "${ORIG_PULL_CONFIG}"
-    fi
-
-    echo 'Git user name and email credentials reset to original state' 1>&1
-    echo 'Git pull config reset to original state' 1>&1
-}
-
-### functions above ###
-### --------------- ###
-### script below    ###
-
-# fail if upstream_repository is not set in workflow
-if [ -z "${INPUT_UPSTREAM_REPOSITORY}" ]; then
-    echo 'Workflow missing input value for "upstream_repository"' 1>&2
-    echo '      example: "upstream_repository: aormsby/fork-sync-with-upstream-action"' 1>&2
-    exit 1
-else
-    UPSTREAM_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_UPSTREAM_REPOSITORY}.git"
-    DOWNSTREAM_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/kantanhq/corgi-gi.git"
-fi
-
-# set user credentials in git config
-config_git
-
-# ensure target_branch is checked out
-# if [ $(git branch --show-current) != "${INPUT_TARGET_BRANCH}" ]; then
-    git checkout ${INPUT_GIT_CHECKOUT_ARGS} "${INPUT_TARGET_BRANCH}"
-    echo 'Target branch ' ${INPUT_TARGET_BRANCH} ' checked out' 1>&1
-# fi
-
-git remote rm origin
-git remote add origin "${DOWNSTREAM_REPO}"
+UPSTREAM_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_UPSTREAM_REPOSITORY}.git"
 
 # set upstream to upstream_repository
 git remote add upstream "${UPSTREAM_REPO}"
 git remote set-url --push upstream DISABLE
 
-# check remotes in case of error
-git remote -v
+# Fetch upstream changes
+git fetch upstream
 
-echo 'checkout to master starting' 1>&1
-git checkout master git merge upstream/master
-git status
-echo 'checkout to master complete' 1>&1
+# Prune deleted branches
+git fetch -p
+
+# Swap to master branch
+git checkout master
+
+# Merge in the changes from upstream/master
+git merge upstream/master
+
+# Check if any branches need pruning
+git branch --merged master | grep -v 'master$'
+
+if [ $? -eq 1 ]; then
+    echo "No local branches need deleting"
+else
+    echo "Local branches need deleting"
+    git branch --merged master | grep -v 'master$' | xargs git branch -d
+fi
+
+# Update our fork master with any changes
+# git push
+
+echo "Finished!"
+
+# set -e
+# # do not quote GIT_PULL_ARGS or GIT_*_ARGS. As they may contain
+# # more than one argument.
+
+# # set user credentials in git config
+# config_git() {
+#     # store original user config for reset later
+#     ORIG_USER=$(git config --global --get --default="null" user.name)
+#     ORIG_EMAIL=$(git config --global --get --default="null" user.email)
+#     ORIG_PULL_CONFIG=$(git config --global --get --default="null" pull.rebase)
+
+#     if [ "${INPUT_GIT_USER}" != "null" ]; then
+#         git config --global user.name "${INPUT_GIT_USER}"
+#     fi
+
+#     if [ "${INPUT_GIT_EMAIL}" != "null" ]; then
+#         git config --global user.email "${INPUT_GIT_EMAIL}"
+#     fi
+
+#     if [ "${INPUT_GIT_PULL_REBASE_CONFIG}" != "null" ]; then
+#         git config --global pull.rebase "${INPUT_GIT_PULL_REBASE_CONFIG}"
+#     fi
+
+#     echo 'Git user and email credentials set for action' 1>&1
+# }
+
+# # reset user credentials to originals
+# reset_git() {
+#     if [ "${ORIG_USER}" = "null" ]; then
+#         git config --global --unset user.name
+#     else
+#         git config --global user.name "${ORIG_USER}"
+#     fi
+
+#     if [ "${ORIG_EMAIL}" = "null" ]; then
+#         git config --global --unset user.email
+#     else
+#         git config --global user.email "${ORIG_EMAIL}"
+#     fi
+
+#     if [ "${ORIG_PULL_CONFIG}" = "null" ]; then
+#         git config --global --unset pull.rebase
+#     else
+#         git config --global pull.rebase "${ORIG_PULL_CONFIG}"
+#     fi
+
+#     echo 'Git user name and email credentials reset to original state' 1>&1
+#     echo 'Git pull config reset to original state' 1>&1
+# }
+
+# ### functions above ###
+# ### --------------- ###
+# ### script below    ###
+
+# # fail if upstream_repository is not set in workflow
+# if [ -z "${INPUT_UPSTREAM_REPOSITORY}" ]; then
+#     echo 'Workflow missing input value for "upstream_repository"' 1>&2
+#     echo '      example: "upstream_repository: aormsby/fork-sync-with-upstream-action"' 1>&2
+#     exit 1
+# else
+#     UPSTREAM_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_UPSTREAM_REPOSITORY}.git"
+#     DOWNSTREAM_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/kantanhq/corgi-gi.git"
+# fi
+
+# # set user credentials in git config
+# config_git
+
+# # ensure target_branch is checked out
+# # if [ $(git branch --show-current) != "${INPUT_TARGET_BRANCH}" ]; then
+#     git checkout ${INPUT_GIT_CHECKOUT_ARGS} "${INPUT_TARGET_BRANCH}"
+#     echo 'Target branch ' ${INPUT_TARGET_BRANCH} ' checked out' 1>&1
+# # fi
+
+# git remote rm origin
+# git remote add origin "${DOWNSTREAM_REPO}"
+
+# # set upstream to upstream_repository
+# git remote add upstream "${UPSTREAM_REPO}"
+# git remote set-url --push upstream DISABLE
+
+# # check remotes in case of error
+# git remote -v
+
+# echo 'checkout to master starting' 1>&1
+# git checkout master git merge upstream/master
+# git status
+# echo 'checkout to master complete' 1>&1
 
 # echo 'fetch starting' 1>&1
 # # # check latest commit hashes for a match, exit if nothing to sync
