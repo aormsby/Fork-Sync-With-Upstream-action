a89a4ecc4ceb33690b42b9ee2aac49ed6d827b6c
GH Action - Upstream Sync <action@github.com>
2021-07-19 20:23:15 -0500

    Fix tests for source branch, upstream repo exist

--- test/verify_branches.sh
+++ test/verify_branches.sh
@@ -15,9 +15,10 @@ test_target_branch_exists() {
 test_upstream_repo_exists() {
     write_out "y" "TEST"
     write_out -1 "[Verify Upstream Sync Repo Exists] -> tests 'upstream_sync_repo' input"
-    VERIFY_TARGET_REPO=$(git ls-remote "${UPSTREAM_REPO_URL}")
+    VERIFY_TARGET_REPO=$(git ls-remote "${UPSTREAM_REPO_URL}" --quiet)
 
-    if [ -z "${VERIFY_TARGET_REPO}" ]; then
+    # var is empty on success, so fail if return value has any data
+    if [ -n "${VERIFY_TARGET_REPO}" ]; then
         write_out "r" "FAILED - Upstream repo '${INPUT_UPSTREAM_SYNC_REPO}' not found OR you do not have permission to view it\n"
     else
         write_out "g" "PASSED\n"
@@ -28,8 +29,9 @@ test_upstream_repo_exists() {
 test_upstream_branch_exists() {
     write_out "y" "TEST"
     write_out -1 "[Verify Upstream Sync Branch Exists] -> tests 'upstream_sync_branch' input"
-    VERIFY_UPSTREAM_BRANCH=$(git ls-remote "${UPSTREAM_REPO_URL}" --refs "${INPUT_UPSTREAM_SYNC_BRANCH}")
+    VERIFY_UPSTREAM_BRANCH=$(git ls-remote "${UPSTREAM_REPO_URL}" "${INPUT_UPSTREAM_SYNC_BRANCH}" --quiet)
 
+    # var contains the ref on success, so fail if return value is empty
     if [ -z "${VERIFY_UPSTREAM_BRANCH}" ]; then
         write_out "r" "FAILED - no branch '${INPUT_UPSTREAM_SYNC_BRANCH}' found on remote repo\n"
     else
