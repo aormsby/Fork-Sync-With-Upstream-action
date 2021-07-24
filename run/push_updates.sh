e9f02b6c7213d5f955699e4a64c6ab47c027e7bf
Adam Ormsby <ormsbyadam@gmail.com>
2021-07-24 18:33:50 -0500

    Udpate README for v3

--- run/push_updates.sh
+++ run/push_updates.sh
@@ -4,7 +4,7 @@
 push_new_commits() {
     write_out -1 'Pushing synced data to source branch.'
 
-    # TODO: figure out hwo this would work in local mode...
+    # TODO: figure out how this would work in local mode...
     # update origin url with token since it is not persisted during checkout step when syncing from a private repo
     if [ -n "${INPUT_UPSTREAM_REPO_ACCESS_TOKEN}" ]; then
         git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_TARGET_REPO_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
