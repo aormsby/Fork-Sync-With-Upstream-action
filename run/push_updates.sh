aa1ee5d4f968eff69802e9a63faebcac1734e273
Adam Ormsby <ormsbyadam@gmail.com>
2021-08-22 20:21:28 -0500

    Always set origin remote url using actor/token

--- run/push_updates.sh
+++ run/push_updates.sh
@@ -5,10 +5,8 @@ push_new_commits() {
     write_out -1 'Pushing synced data to target branch.'
 
     # TODO: figure out how this would work in local mode...
-    # update origin url with token since it is not persisted during checkout step when syncing from a private repo
-    if [ -n "${INPUT_UPSTREAM_REPO_ACCESS_TOKEN}" ]; then
-        git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_TARGET_REPO_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-    fi
+    # update origin url with token since it is not necessarily persisted during checkout step
+    git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_TARGET_REPO_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
     # shellcheck disable=SC2086
     git push ${INPUT_TARGET_PUSH_ARGS} origin "${INPUT_TARGET_SYNC_BRANCH}"
