3b3ef9d74415c2815238a0456e5b9dfc2e0f861c
Adam Ormsby <ormsbyadam@gmail.com>
2021-10-31 15:50:25 -0500

    Re-enable rest of action

--- entry/run_action.sh
+++ entry/run_action.sh
@@ -25,11 +25,11 @@ find_last_synced_commit
 output_new_commit_list
 sync_new_commits
 
-# # shellcheck disable=SC1091
-# # push newly synced commits to local branch
-# . "${ACTION_PARENT_DIR}"/run/push_updates.sh
-# push_new_commits
+# shellcheck disable=SC1091
+# push newly synced commits to local branch
+. "${ACTION_PARENT_DIR}"/run/push_updates.sh
+push_new_commits
 
-# # git config cleanup for workflow continuation
-# # function from config_git.sh
-# reset_git_config
+# git config cleanup for workflow continuation
+# function from config_git.sh
+reset_git_config
